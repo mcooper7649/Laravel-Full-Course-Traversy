e17da5a1e538e34dab0e9e95ca4c417ab965d80b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-15 09:36:51 -0400

    created seeder listings and users

---
+++ data.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    [
+        'title' => 'Laravel Senior Developer',
+        'tags'  => 'laravel, javascript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston, MA',
+        'email' => 'I8Hb6@example.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?'
+    ],
+    [
+        'title' => 'React.js Developer',
+        'tags'  => 'react, javascript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston, MA',
+        'email' => 'user12@example.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?'   
+    ],
+    [
+        'title' => 'Laravel Developer',
+        'tags'  => 'laravel, javascript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston, MA',
+        'email' => 'random@example.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?'
+    ],
+]
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Listing;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(5)->create();
+
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'I8Hb6@example.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
+        ]);
+
+        Listing::create([
+            'title' => 'React.js Developer',
+            'tags' => 'react, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'user12@example.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
+        ]);
+
+        Listing::create([
+            'title' => 'Laravel Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'random@example.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
         ]);
     }
 }
+
+// User::factory()->create([
+//     'name' => 'Test User',
+//     'email' => 'test@example.com',
+// ]);
