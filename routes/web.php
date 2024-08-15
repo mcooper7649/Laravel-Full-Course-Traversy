52b8f0d7a077251e62af846fbc4ce2b64d4d9ec0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-15 11:32:02 -0400

    blade components, component attriblutes and tags component

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Latest Listings',
         'listings' => Listing::all(),
     ]);
 });
@@ -15,8 +14,6 @@ Route::get('/', function () {
 // Single Listing
 
 Route::get('/listings/{listing}', function (Listing $listing) {
-
-    $listing = Listing::find($id);
 
     return view('listing', [
         'listing' => $listing,
