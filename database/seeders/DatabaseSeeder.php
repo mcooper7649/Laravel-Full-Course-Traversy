b06fcce771eb1505647fa20a95971751b132cbdd
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-15 10:25:40 -0400

    created listing factory, added layout, sections and theme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,35 +17,37 @@ class DatabaseSeeder extends Seeder
 
         User::factory(5)->create();
 
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'I8Hb6@example.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
-        ]);
+        Listing::factory(6)->create();
 
-        Listing::create([
-            'title' => 'React.js Developer',
-            'tags' => 'react, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'user12@example.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
-        ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'I8Hb6@example.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
+        // ]);
 
-        Listing::create([
-            'title' => 'Laravel Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'random@example.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
-        ]);
+        // Listing::create([
+        //     'title' => 'React.js Developer',
+        //     'tags' => 'react, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'user12@example.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'random@example.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quasi?',
+        // ]);
     }
 }
 
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,7 +1,10 @@
+@extends('layout')
 
-    <h2>
-        {{$listing['title']}}
-    </h2>
-    <p>
-        {{$listing['description']}}
-    </p>
+@section('content')
+        <h2>
+            {{$listing['title']}}
+        </h2>
+        <p>
+            {{$listing['description']}}
+        </p>
+@endsection
