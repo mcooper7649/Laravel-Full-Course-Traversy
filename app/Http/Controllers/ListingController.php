95ba748687956bdc62cda6495708d094b3d980b0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-19 11:55:00 -0400

    added delete

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -83,4 +83,13 @@ class ListingController extends Controller
 
         return back()->with('message', 'Listing updated successfully!');
     }
+
+    //Delete listing
+
+    public function destroy(Listing $listing)
+    {
+        $listing->delete();
+
+        return redirect('/')->with('message', 'Listing deleted successfully!');
+    }
 }
