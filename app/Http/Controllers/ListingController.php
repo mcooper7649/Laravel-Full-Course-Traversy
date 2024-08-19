9079d4380aebb939eee19d48a393653397764475
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-19 11:45:28 -0400

    added file update and edit view and functionality, updated model

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -46,8 +46,41 @@ class ListingController extends Controller
             'description' => 'required',
         ]);
 
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
+    }
+
+    //Show edit form
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    //Update listing
+
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = request()->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'email' => ['required', 'email'],
+            'website' => 'required',
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
     }
 }
